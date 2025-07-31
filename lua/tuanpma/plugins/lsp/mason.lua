e125161613ae6033a18779899837cdfc6472e511
Phan Minh Anh Tuan <tuanphanminhanh@gmail.com>
2025-08-01 02:16:11 +0900

    add terminal

--- lua/tuanpma/plugins/lsp/mason.lua
+++ lua/tuanpma/plugins/lsp/mason.lua
@@ -1,41 +1,44 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
-    local mason_tool_installer = require("mason-tool-installer")
-
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
         },
-      },
-    })
-
-    -- minimal mason-lspconfig setup - just for installation
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "pyright", 
-        "clangd",
-      },
-    })
-
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "stylua", -- lua formatter
-        "black", -- python formatter
-        "pylint",
-        "isort", -- python formatter
-      },
-    })
-  end,
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "clangd",
+        },
+        automatic_installation = false,
+      })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "stylua", -- lua formatter
+          "black", -- python formatter
+          "pylint",
+          "isort", -- python formatter
+        },
+      })
+    end,
+  },
 }
